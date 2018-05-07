077a1e37da794ece7808b6bc90cd8befbc4ccd50
Zaphod Beeblebrox <38865576+Sirius-Cybernetics-Corporation@users.noreply.github.com>
2018-05-06 17:29:49 -0700

    Added initial .js file
    Currently is able to get summoner name and api key from user. Generates game data, pulls champion ids, randomly selects one champion ID, then retrieves that champions name.

---
+++ auto_boost.js
@@ -0,0 +1,270 @@
+// TT-3000 LoL Auto Songbooster v1.0.0
+// David Grey, 
+// May 4, 2018
+// Description: Website to take summoner name input->get summoner id->get current game info->get
+//  team champs->randomly select one champ->generate pre-defined song boost link for that champ. 
+
+
+// DEVELOPMENT INSTRUCTIONS (Windows)
+// Install node.js in windows https://nodejs.org/en/download/
+// In cmd type npm install request
+// Run the program:
+//    Add javascript build to sublime text: https://pawelgrzybek.com/javascript-console-in-sublime-text/
+//    press ctrl+b to run program in sublime text
+//  OR 
+//    in cmd navigate to the directory of your .js file
+//    in cmd run node api_get_data.js
+// In chrome, type http://localhost:1339/ or http://127.0.0.1:1339/ and press enter
+// Note: if you try to run 
+
+
+// HELPFUL REFERENCES: 
+// HTML Client side inside of server side js file: http://techslides.com/client-side-javascript-to-node-js
+// Getting variables from ajax request: https://stackoverflow.com/questions/15211186/getting-variable-out-of-a-get-ajax-call?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+// Pulling all instances of a string in a string (regex): https://stackoverflow.com/questions/3365902/search-for-all-instances-of-a-string-inside-a-string 
+
+
+// TASKLIST (Immediate)
+// - 50% chance it might boost the wrong team. I pull the first 5 champ id's from the game data, without realizing that might be the other team. (you are either 1-5 or 6-10)
+// - Figure out how to extra data (summ_id, line 182) out of calc function to a usable variable
+// - Pull song link from csv file or database
+// - Post song link (or embed video? or open in new window?)
+// -
+
+// TASKLIST (Backburner & Feature upgrades)
+// - checkbox to omit Vitas (champs without a designated song)
+// - tick counter at bottom of page to show (decimal) millions of customers served
+// - add error handling (if exceed api request limits, invalid data, etc)
+//
+// ******************************************* VARIABLES ***********************************
+
+
+
+var http = require('http'); 
+var request = require('request');
+var url_summ_id = "blank";
+
+
+
+// ******************************************* USER INPUT FORM ********************************
+// Create a function to handle every HTTP request
+function handler(req, res){
+
+  var form = '';
+
+  if(req.method == "GET"){ 
+    
+    form = '<!doctype html> \
+<html lang="en"> \
+<head> \
+    <meta charset="UTF-8">  \
+    <title>LoL Song Boost</title> \
+    <body style="background-color: #000;}">\
+</head> \
+<body> \
+<!--*********************************************** STYLE ********************************************-->\
+<style>\
+  p {\
+    color: #00FF80;\
+    font-family: "Courier New", Courier, monospace;\
+    font-size: 100%;\
+    text-align: center;\
+}\
+\
+  input[type=text] {\
+    outline: none;\
+    font-family: "Courier New", Courier, monospace;\
+    padding: 2px 2px;\
+    margin: 4px 0;\
+    box-sizing: border-box;\
+    border: 2px solid #00FF80;\
+    background-color: #000;\
+    color: #00FF80;\
+    text-align: center;\
+}\
+input:-webkit-autofill {\
+    -webkit-box-shadow: 0 0 0px 1000px black inset;\
+    -webkit-text-fill-color: green !important;\
+}\
+\
+.button1 {\
+    font-family: "Courier New", Courier, monospace;\
+    padding: 15px 32px;\
+    text-align: center;\
+    display:inline-block;\
+    margin: 4px 2px;\
+    background-color: #000; \
+    color: #00FF80; \
+    border: 2px solid #00FF80;\
+    cursor: pointer;\
+}\
+\
+</style>\
+<!--*********************************************** PROGRAM ********************************************-->\
+  <form name="myForm" action="" onsubmit="return ajax();"method="post">\
+      <br>\
+      <p> summoner name: <input type="text" name="A"> </p> \
+      <p> rito api key: <input type="text" name="B"> </p>\
+      <br>\
+      <p> <input type="submit" class="button1" value="song boost!"> </p>\
+      <p> <span id="result"></span> </p> \
+      <P> <span id="result2"></span> </p> \
+  </form> \
+  <script> \
+    function ajax(){ \
+      var a = document.forms["myForm"]["A"].value; \
+      var b = document.forms["myForm"]["B"].value; \
+      var formdata = a+"&"+b; \
+      \
+      xmlhttp = new XMLHttpRequest(); \
+      xmlhttp.onreadystatechange=function(){ \
+        if(xmlhttp.readyState==4 && xmlhttp.status==200){ \
+          document.getElementById("result").innerHTML=xmlhttp.responseText; \
+        }; \
+      }; \
+      xmlhttp.open("POST","",true); \
+      xmlhttp.send(formdata); \
+      return false; \
+    } \
+  </script> \
+</body> \
+</html>';
+
+// *********************************************** END HTML ********************************************
+  //respond
+  res.setHeader('Content-Type', 'text/html');
+  res.writeHead(200);
+  res.end(form);
+  
+
+  } else if(req.method == 'POST'){
+
+    //read form data
+    req.on('data', function(chunk) {
+
+      //grab form data as string
+      var formdata = chunk.toString();
+
+      //grab A and B values
+      var a = formdata.split("&")[0];
+      var b = formdata.split("&")[1];
+      //replace spaces with %20 so url will work. E.g., Pro Poop Eater would be Pro%20Poop%20Eater
+      //g flag is to replace all instances
+      var a_mod = a.replace(/ /g, "%20");
+
+
+
+      result = get_api_data(a,a_mod,b);
+      // console.log("result is:" + result);
+
+      //fill in the result and form values
+      form = result.toString();
+
+      //respond
+      res.setHeader('Content-Type', 'text/html');
+      res.writeHead(200);
+      res.end(form);
+
+    });
+
+  } else {
+    res.writeHead(200);
+    res.end();
+  };
+
+};
+
+// *********************************************** GET API DATA ********************************************
+// *********************************************** STEP 1 - SUMM ID ****************************************
+function get_api_data(a,a_mod,b)
+{
+  url_summ_id = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + a_mod + '?api_key=' + b;
+
+  request(url_summ_id, function(err, resp, html)
+    {
+        if (!err)
+        {
+         //log the data from the request
+          // console.log("\n Data from API request: ");
+          // console.log(html); 
+
+          //Parse the data
+          obj = JSON.parse(html);
+
+          //extract the summoner ID from the data & set to a variable
+          console.log("\nSummoner ID: ");
+          summ_id = obj.id;
+          console.log(summ_id);
+
+// ******************************* STEP 2 - GAME INFO TO CHAMP ID'S TO RANDOM CHAMP ID ****************************************
+          url_game_data='https://na1.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/'+ summ_id + '?api_key=' + b;
+
+          request(url_game_data, function(err, resp, html2)
+            {
+                if (!err)
+                {
+                 //log the data from the request
+                  // console.log("\n Data from API request: ");
+                  // console.log(html2); 
+
+                  //Parse the data
+                  obj2 = JSON.parse(html2);
+
+                  // turn json data into a string
+                  var json_string = JSON.stringify(obj2);
+                  // cleanup by removing quotes and brackets, might not be needed. 
+                  var json_string_2 = json_string.replace(/['"]+/g, '');
+                  var json_string_3 = json_string_2.replace(/[{}]+/g, '');
+                  //pull champion id's followed by 3 digits and store into an array
+                  let all_champ_ids = json_string_3.match(/championId:\d\d?\d?/g);
+                  //pull the first 5 champs (your team only)
+                  var only_tryhards = all_champ_ids.slice(0,5);
+                  //select a random champ
+                  random_champ = only_tryhards[Math.floor(Math.random()*only_tryhards.length)];
+                  //keep only the champion number (remove "ChampionID" text)
+                  random_champ_id = random_champ.replace(/\D/g,'');
+                  console.log("\nRandom championId from team: ")
+                  console.log(random_champ_id);
+
+// ***************************** STEP 3 - GET CHAMP NAME FROM RANDOM ID **************************
+                  url_champ_name = 'https://na1.api.riotgames.com/lol/static-data/v3/champions/' + random_champ_id + '?locale=en_US&api_key=' + b;
+
+                  request(url_champ_name, function(err, resp, html3)
+                  {
+                      if (!err)
+                      {
+                        //Parse the data
+                        obj3 = JSON.parse(html3);
+                        champ_name = obj3.name;
+                        console.log("\nChampion: " + champ_name + " Gets the boost");
+                        console.log(summ_id);
+
+                      }
+                  });
+
+// ***************************** STEP 3 - END STEP 3**********************************************
+
+
+                }
+              });
+
+        }
+    });
+
+  return  "Output to client: Summoner Name: " + a + " API_KEY: " + b;
+
+}
+
+
+
+
+// Create a server that invokes the `handler` function upon receiving a request
+http.createServer(handler).listen(1339, function(err){
+  if(err){
+    console.log('Error starting http server');
+  } else {
+    console.log("Server running at http://127.0.0.1:1339/ or http://localhost:1339/");
+  };
+});
+
+
