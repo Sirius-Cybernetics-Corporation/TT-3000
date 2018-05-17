86259d5d1f31b04305a9a2d119d7635693e115e2
Zaphod Beeblebrox <38865576+Sirius-Cybernetics-Corporation@users.noreply.github.com>
2018-05-17 12:22:30 -0700

    Wrong Team Boost Fix
    Fixed wrong team boost by extracting data based on team number (100 or 200) and choosing champs from that data.

--- auto_boost.js
+++ auto_boost.js
@@ -25,6 +25,7 @@
 
 
 // TASKLIST (Immediate)
+// - Team ID comes up correctly in test.js, but not in auto_boost.js
 // - 50% chance it might boost the wrong team. I pull the first 5 champ id's from the game data, without realizing that might be the other team. (you are either 1-5 or 6-10)
 // - Figure out how to extra data (summ_id, line 182) out of calc function to a usable variable
 // - Pull song link from csv file or database
@@ -196,6 +197,8 @@ function get_api_data(a,a_mod,b)
           summ_id = obj.id;
           console.log(summ_id);
 
+
+
 // ******************************* STEP 2 - GAME INFO TO CHAMP ID'S TO RANDOM CHAMP ID ****************************************
           url_game_data='https://na1.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/'+ summ_id + '?api_key=' + b;
 
@@ -208,23 +211,51 @@ function get_api_data(a,a_mod,b)
                   // console.log(html2); 
 
                   //Parse the data
-                  obj2 = JSON.parse(html2);
+                  json_obj2 = JSON.parse(html2);
 
                   // turn json data into a string
-                  var json_string = JSON.stringify(obj2);
-                  // cleanup by removing quotes and brackets, might not be needed. 
-                  var json_string_2 = json_string.replace(/['"]+/g, '');
-                  var json_string_3 = json_string_2.replace(/[{}]+/g, '');
-                  //pull champion id's followed by 3 digits and store into an array
-                  let all_champ_ids = json_string_3.match(/championId:\d\d?\d?/g);
-                  //pull the first 5 champs (your team only)
-                  var only_tryhards = all_champ_ids.slice(0,5);
-                  //select a random champ
-                  random_champ = only_tryhards[Math.floor(Math.random()*only_tryhards.length)];
-                  //keep only the champion number (remove "ChampionID" text)
-                  random_champ_id = random_champ.replace(/\D/g,'');
-                  console.log("\nRandom championId from team: ")
-                  console.log(random_champ_id);
+                  var json_string = JSON.stringify(json_obj2)
+
+                  // ************************************* GET TEAM NUMBER ****************************************
+                  // find out what team you are on
+                  //EXTRACT EVERTHING FROM BEGINNING TO SUMMONER NAME
+                  var chop1 = json_string.substr(0, json_string.indexOf(a));
+                  // GET START/END INDEXES FOR TEAM ID and add or subtract to get only the team number. 
+                  var index_start = chop1.lastIndexOf("teamId") + 8;
+                  // CREATE 2ND INDEX FOR DIGITS AFTER TEAMID 
+                  var index_end = chop1.lastIndexOf("spell1Id") - 2;
+                  // EXTRACT TEAM ID
+                  var team_ID = chop1.slice(index_start,index_end);
+                  console.log("Team ID: " + team_ID);
+                  // might be useful for pulling a string out of an array
+                  // splitString[1];
+// ************************************** END TEAM GET NUMBER************************************
+
+// ************************************* GET CHAMP ID'S ON TEAM *********************************
+                  if (team_ID == "100"){
+                    console.log("(blue team)")
+                    var purple_start = json_string.indexOf("teamId\":" + team_ID)
+                    var purple_end = json_string.indexOf("teamId\":200")
+                    json_team = json_string.slice(purple_start,purple_end);
+                    // console.log(json_purple);
+                  }
+                  else if (team_ID == "200"){
+                    console.log("purple team")
+                    var blue_start = json_string.indexOf("teamId\":" + team_ID)
+                    var blue_end = json_string.indexOf("observers")
+                    json_team = json_string.slice(blue_start,blue_end);
+                  }
+                  else{
+                    console.log("not a valid team number")
+                  }
+// ********************************* END CHAMP ID'S ON TEAM ************************
+
+                  // ************************************* GET CHAMP NUMBERS ****************************************
+                  let tryhard_team = json_team.match(/championId":\d\d?\d?/g);
+                  random_champ_str = tryhard_team[Math.floor(Math.random()*tryhard_team.length)];
+                  random_champ_id = random_champ_str.replace(/\D/g,'');
+                  console.log("Random Champ ID: " + random_champ_id);
+                  // ************************************* END GET CHAMP NUMBERS ****************************************
 
 // ***************************** STEP 3 - GET CHAMP NAME FROM RANDOM ID **************************
                   url_champ_name = 'https://na1.api.riotgames.com/lol/static-data/v3/champions/' + random_champ_id + '?locale=en_US&api_key=' + b;
